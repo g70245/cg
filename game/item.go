0dfd9e0b350a48f56b14cd0edcc4ece4bcf1adba
Leon Peng <g70245@gmail.com>
2023-09-24 18:40:10 +0800

    Add a new item

--- game/item.go
+++ game/item.go
@@ -12,14 +12,16 @@ type Item struct {
 type Items []Item
 
 const (
+	N_7B = "7B"
 	N_8B = "8B"
 	N_9A = "9A"
 )
 
 var (
+	I_B_7B       = Item{N_7B, COLOR_ITEM_BOMB_7B}
 	I_B_8B       = Item{N_8B, COLOR_ITEM_BOMB_8B}
 	I_B_9A       = Item{N_9A, COLOR_ITEM_BOMB_9A}
-	Bombs  Items = []Item{I_B_8B, I_B_9A}
+	Bombs  Items = []Item{I_B_7B, I_B_8B, I_B_9A}
 )
 
 func (is Items) GetOptions() []string {
